be400fd15143c91117125ab5148f6088f1accd1c
Rodrigo dos Santos Coelho <rscoelho.dev@gmail.com>
2026-07-26 20:46:50 +0100

    fix: request mapping typo

--- order-service/src/main/java/com/orderflow/order/controller/AddressController.java
+++ order-service/src/main/java/com/orderflow/order/controller/AddressController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/adresses")
+@RequestMapping("/api/addresses")
 @RequiredArgsConstructor
 public class AddressController {
 
